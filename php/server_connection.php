b0330069ed215a5ea0e41d4ffeca9c3e87440100
CoderBryan <angelbryanreyes07@gmail.com>
2022-08-08 03:57:43 +0800

    server_commit

--- php/server_connection.php
+++ php/server_connection.php
@@ -1,8 +1,8 @@
 <?php
 error_reporting(0);
-$servername = 'localhost';
-$username = 'root';
-$password = '';
-$dbname = 'may_db';
+$servername = 'x71wqc4m22j8e3ql.cbetxkdyhwsb.us-east-1.rds.amazonaws.com';
+$username = 's8z1rccahkocpqs7';
+$password = 'c6kdlysaxjk60wa4';
+$dbname = 'ueihmh18ojbfew0f';
 
 $conn = mysqli_connect('localhost', 'root', '', 'may_db');
